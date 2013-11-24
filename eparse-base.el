239cf6d83fcdaaf98f7f2cda9d89878f8cb96573
derui <derutakayu@gmail.com>
2013-11-24 23:51:06 +0900

    Refine eplib:define-lexer that provide DSL to define lexer with eparse-base library.

--- eparse-base.el
+++ eparse-base.el
@@ -9,7 +9,7 @@
 ;; Created: Nov 13 2013
 ;; Version: 0.0.0
 ;; Keywords: parser library
-;; Compatibility: 24.0 <=
+;; Compatibility: 24.3 <=
 ;; 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; 
@@ -44,86 +44,168 @@
 ;; 
 ;;; Code:
 
-(defun eplib:return (input readsize)
-  "Return next input for next lexing that use with it."
-  (if (and (numberp readsize)
-           (<= 0 readsize))
-      (let ((pos (eplib:position-of-input input))
-            (text (eplib:text-of-input input)))
-        (list
-         (substring text pos readsize)
-         (eplib:make-input (+ readsize pos) (substring text (+ readsize pos))))
-        )
-    (list "" input)
-  ))
+(require 'cl-lib)
 
-(defun eplib:success (input readsize)
-  "Return next input for next lexing with it, and add tag of success"
-  (let ((next (eplib:return input readsize)))
-    (cons 'success next))
-  )
+;; Definitions for functions are support to parse and lex.
+(defun eplib:successp (v)
+  "Check the value if it equal a type of success"
+  (and v
+       (listp v)
+       (eq 'success (car v))))
 
-(defun eplib:fail (message)
-  "Return fail tag and empty input informations.
+(defun eplib:failp (v)
+  "Check the value if it equal a type of failure"
+  (and v
+       (listp v)
+       (eq 'fail (car v))))
+
+(defun eplib:success-val (v)
+  (when (eplib:successp v) (cadr v)))
+
+(defun eplib:fail-val (v)
+  (when (eplib:failp v) (cdr v)))
+
+(defun eplib:pull-source (ret)
+  "Pull source in a value with `eplib:success'."
+  (when (eplib:successp ret)
+    (nth 2 ret)))
+
+(defun eplib:success (source val)
+  "Return a value which is result of some funciton and consumed source"
+  (list 'success val source))
+
+(defun eplib:fail (val)
+  "Return fail tag and empty source informations.
 If you use it that is returned this funciton, any lexer with fail."
-  (cons 'fail message))
+  (cons 'fail val))
 
-(defun eplib:eitherp (pred)
-  "Check argument if it is either structure or not"
-  (and (listp pred)
-       (or
-        (and (eq 'success (car pred))
-             (= 3 (length pred)))
-        (and (eq 'fail (car pred))
-             (consp pred)))))
+(defun eplib:make-pos ()
+  "Get initialized source position which is point of the document while parsing."
+  '(0 0 0))
 
-(defun eplib:pos-init (&optional position)
-  "Get initialized source position which is point of the document while parsing.
-If position is nil, return position that is start of the document which column 0 and line 0.
-You can send argument as assosiate list which having 'col and 'line key."
-  (if (and position
-           (listp position))
-      (let* ((col (when (assoc 'col position)
-                   (cdr (assoc 'col position))))
-             (line (when (assoc 'line position)
-                     (cdr (assoc 'line position))))
-             (col (if (numberp col) col 0))
-             (line (if (numberp line) line 0)))
-        (list col line))
-    '(0 0)))
+(defun eplib:posp (pos)
+  (and pos
+       (listp pos)
+       (= 3 (length pos))
+       (numberp (car pos))
+       (numberp (cadr pos))
+       (numberp (nth 2 pos))))
+
+(defun eplib:pos-position (pos)
+  "Get current position at the document. This value is absolute position of it, and it is zero origin."
+  (cond ((eplib:posp pos)
+         (nth 0 pos))
+        (t
+         -1)))
 
 (defun eplib:pos-col (pos)
   "Get current column in the given position at the document.
 If given argument is not valid position or illegal column, return -1 as error"
-  (cond ((and pos (listp pos) (numberp (car pos)))
-         (car pos))
+  (cond ((eplib:posp pos)
+         (nth 1 pos))
         (t
          -1)))
 
 (defun eplib:pos-line (pos)
   "Get current line in the given position at the document.
 If given argument is not valid position or illegal line, return -1 as error"
-  (cond ((and pos (listp pos) (numberp (cadr pos)))
-         (cadr pos))
+  (cond ((eplib:posp pos)
+         (nth 2 pos))
         (t -1)))
 
-(defun eplib:pos-update (pos newer)
+(defun eplib:update-pos (pos newer &optional info)
   "Return new position with newer column and line which are updating explicitly as
 associate list. Original position is not updating with destructive."
-  (if (and pos newer (listp newer))
-      (let* ((col (car pos))
-             (line (cadr pos))
-             (new-col (and (numberp (cdr (assoc 'col newer)))
-                           (cdr (assoc 'col newer))))
-             (new-line (and (numberp (cdr (assoc 'line newer)))
-                            (cdr (assoc 'line newer)))))
-        (list (if new-col new-col col)
+  (if (and pos newer (numberp newer))
+      (let* ((col (nth 1 pos))
+             (line (nth 2 pos))
+             (new-col (and (numberp (cdr (assoc 'col info)))
+                           (cdr (assoc 'col info))))
+             (new-line (and (numberp (cdr (assoc 'line info)))
+                            (cdr (assoc 'line info)))))
+        (list newer
+              (if new-col new-col col)
               (if new-line new-line line)))
     pos))
 
+(defun eplib:make-source (text)
+  "Make and return source structure with initial position and text"
+  (cons (eplib:make-pos) text))
+
+(defun eplib:sourcep (source)
+  (and source
+       (consp source)
+       (eplib:posp (car source))
+       (stringp (cdr source))))
+
+(defun eplib:source-pos (source)
+  "Get position that is current reading point from text from argument."
+  (and (eplib:sourcep source)
+       (car source)))
+
+(defun eplib:source-text (source)
+  "Get text from argument"
+  (and (eplib:sourcep source)
+       (cdr source)))
+
+(defun eplib:update-source (source pos)
+  "Update position of source and return newer it."
+  (when (eplib:sourcep source) (eplib:posp pos)
+        (cons pos (eplib:source-text source))))
+
+(defun eplib:read-from-source (source readsize)
+  "Read text start from current position of the source to readsize.
+Note, this function no any effect to `source'. You have to use `eplib:pos-update' and
+`eplib:replace-source-pos' if you want to read to read continuous."
+  (when (eplib:sourcep source)
+    (if (and readsize
+             (numberp readsize)
+             (<= 0 readsize))
+        (let* ((pos (eplib:source-pos source))
+               (abs-pos (eplib:pos-position pos)))
+          (substring (eplib:source-text source) abs-pos (+ abs-pos readsize)))
+      "")))
+
+(defmacro eplib:define-lexer (name argname &rest body)
+  "Define lexer with some features. This macro wrap some function to lexer that is
+execute in expanded macro. The body given this macro have to use eplib:fail and eplib:success form that are
+used to fail or success lex. In the body can use a variable `source' that initialized by eplib:make-source to lex.
+The body must be ended with fail or success with source or data.
+
+This macro provide some utility functions follows to make DSL to define lexer.
+
+(<<- size) : Read string from source with size, this is alias for eplib:read-from-source.
+(forward-pos pos) : Update position forwarding in the source.
+(success result) : this is alias for eplib:success.
+(fail result) : this is alias for eplib:fail.
+"
+  (let (ret)
+  `(defun ,(intern (concat "eplib:lexer:" (symbol-name name))) (,argname)
+     (cl-flet ((<<- (size)
+                    (let* ((pos (eplib:source-pos ,argname))
+                           (abs-pos (eplib:pos-position pos)))
+                      (eplib:read-from-source ,argname size)))
+               (forward-pos (size)
+                            (let* ((pos (eplib:source-pos ,argname))
+                                   (abs-pos (eplib:pos-position pos)))
+                              (setq ,argname
+                                   (eplib:update-source ,argname
+                                                        (eplib:update-pos pos (+ abs-pos size))))))
+               (success (result) (eplib:success ,argname result))
+               (fail (result) (eplib:fail result))
+               )
+       (setq ret ,@body)
+       (if (or (eplib:failp ret) (eplib:successp ret))
+           ret
+         (error "What lexer return success or fail have to behave given body"))
+       ))))
+
+(defmacro eplib:lex (name source)
+  `(,(intern (concat "eplib:lexer:" (symbol-name name))) ,source))
+
 (defun eplib:either (pred next-func)
   "Return new function to execute given function if pred is failed or successful.
-Executing function which is one of function in arguments is took `input' structure,
+Executing function which is one of function in arguments is took `source' structure,
 and expecting it return structure made by `eplib:success' or `eplib:fail'
 If execute function returned with eplib:fail, it return a list equal returning from `eplib:fail'."
   (if (eplib:eitherp pred)
@@ -138,29 +220,14 @@ If execute function returned with eplib:fail, it return a list equal returning f
               ))
     (throw 'not-either-type "Tag is not made from eplib:success or eplib:fail")))
 
-(defun eplib:make-input (pos text)
-  "Make and return input structure with initial position and text"
-  (cons pos text))
-
-(defun eplib:position-of-input (input)
-  "Get position that is current reading point from text from argument."
-  (and (listp input)
-       (car input)))
-
-(defun eplib:text-of-input (input)
-  "Get text from argument"
-  (and (listp input)
-       (cdr input)))
-
-(defmacro eplib:define-lexer (name body)
-  "Define lexer with some features. This macro wrap some function to lexer that is
-execute in expanded macro. The body given this macro have to use eplib:fail and eplib:success form that are
-used to fail or success lex. In the body can use a variable `input' that initialized by eplib:make-input to lex.
-The body must be ended with do-fail or do-success with input or data."
-  `(defun ,(intern (concat "eplib:lexer:" (symbol-name name))) (input)
-     (,@body)))
-
-(defmacro eplib:lex (name input)
-  `(,(intern (concat "eplib:lexer:" (symbol-name name))) ,input))
+(defun eplib:bind (fst snd)
+  "Return new function what combine `fst' to `snd'.
+Notice, arguments of this function has to be able to give only one argument, because
+the result of `fst' executed take to argument of `snd' without change.
+If without function or other give this, return id function that is defined `identity'"
+  (if (and (functionp fst) (functionp snd))
+      #'(lambda (arg)
+          (funcall snd (funcall fst arg)))
+    'identity))
 
 (provide 'eparse-base)
